01a5a61a8f357d1c2dde3c5505842450a04f618f
sus <dangalsushant@gmail.com>
2022-05-19 11:52:59 +0545

    feat: login page created

--- src/components/login.tsx
+++ src/components/login.tsx
@@ -1,19 +1,104 @@
-import { Dialog, DialogTitle, List } from "@mui/material";
+import { ThemeProvider } from "@emotion/react";
+import {
+  Avatar,
+  Box,
+  Button,
+  Checkbox,
+  Container,
+  createTheme,
+  Dialog,
+  DialogTitle,
+  FormControlLabel,
+  Grid,
+  Link,
+  List,
+  TextField,
+  Typography,
+} from "@mui/material";
+import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
+import { FormEvent } from "react";
 
 export interface DialogProps {
   open: boolean;
 }
+const theme = createTheme();
 
 const Login = (props: DialogProps) => {
-  const { open } = props;
+  const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const data = new FormData(event.currentTarget);
+    console.log({
+      email: data.get("email"),
+      password: data.get("password"),
+    });
+  };
 
   return (
-    <>
-      <Dialog open={open}>
-              <DialogTitle>Welcome to login</DialogTitle>
-             
-      </Dialog>
-    </>
+    <ThemeProvider theme={theme}>
+      <Container component="main" maxWidth="xs">
+        <Box
+          sx={{
+            marginTop: 8,
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+          }}
+        >
+          <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
+            <LockOutlinedIcon />
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            Sign in
+          </Typography>
+          <Box
+            component="form"
+            onSubmit={handleSubmit}
+            noValidate
+            sx={{ mt: 1 }}
+          >
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              id="email"
+              label="Email Address"
+              name="email"
+              autoComplete="email"
+              autoFocus
+            />
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              name="password"
+              label="Password"
+              type="password"
+              id="password"
+              autoComplete="current-password"
+            />
+            {/* <FormControlLabel
+              control={<Checkbox value="remember" color="primary" />}
+              label="Remember me"
+            /> */}
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+            >
+              Sign In
+            </Button>
+            <Grid container>
+              <Grid item>
+                <Link href="#" variant="body2">
+                  {"Don't have an account? Sign Up"}
+                </Link>
+              </Grid>
+            </Grid>
+          </Box>
+        </Box>
+      </Container>
+    </ThemeProvider>
   );
 };
 
