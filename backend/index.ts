d5b494ef077ca55353c04da6fffd20a520601c5d
DESKTOP-I754IGB\Sushant Dangal <dangalsushant@gmail.com>
2022-07-03 16:32:56 +0545

    feat: added @types/express-session

--- backend/index.ts
+++ backend/index.ts
@@ -1,8 +1,12 @@
 import express from 'express';
 import http from 'http';
+import mongoose from 'mongoose';
 
 const app = express()
 const httpServer = http.createServer(app);
+
+
+mongoose.connect('mongodb://localhost:27017/myapp');
 
 httpServer.listen(8000, () => {
 console.log(`Server is running at: http//localhost:8000`);
